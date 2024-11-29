5977fccda724e3ee9a1281e26479b751c0a454db
Kalu David <kaludavid411@gmail.com>
2024-11-29 07:46:17 +0100

    another commit

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require("express")
 const dotenv = require("dotenv")
 const morgan = require("morgan")
 const exphbs = require("express-handlebars")
-// const passport = require("passport")
-// const session = require("express-session")
+const passport = require("passport")
+const session = require("express-session")
 const connectDB = require("./config/db")
 
 
@@ -12,9 +12,9 @@ const connectDB = require("./config/db")
 dotenv.config({ path: './config/config.env' })
 
 // Passport config
-// require('./config/passport')(passport)
+require('./config/passport')(passport)
 
-connectDB() 
+connectDB()  
 
 const app = express()
 
@@ -27,23 +27,23 @@ if (process.env.NODE_ENV === 'development') {
 app.engine('.hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
-// Session middleware
-// app.use(session({
-//     secret: 'keyboard cat',
-//     resave: false,
-//     saveUninitialized: false
-// }))
+// Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}))
 
 // Passport middleware
-// app.use(passport.initialize)
-// app.use(passport.session)
+app.use(passport.initialize())
+app.use(passport.session()) 
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
-
+ 
 // Routes
 app.use('/', require('./routes/index'))
-// app.use('/auth', require('./routes/auth'))
+app.use('/auth', require('./routes/auth'))
 
 const PORT = process.env.PORT || 3001
 
