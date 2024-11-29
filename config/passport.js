d55ef584151f648825922afbe66b129c91242c7f
Kalu David <kaludavid411@gmail.com>
2024-11-29 15:42:45 +0100

    another push

--- config/passport.js
+++ config/passport.js
@@ -9,12 +9,37 @@ module.exports = function(passport) {
         callbackURL: '/auth/google/callback'
     },
     async (accessToken, refreshToken, profile, done) => {
-        console.log(profile)
+        const newUser = {
+            googleId: profile.id,
+            displayName: profile.displayName,
+            firstName: profile.name.givenName,
+            lastName: profile.name.familyName,
+            image: profile.photos[0].value
+        }
+
+        try{
+            let user = await User.findOne({ googleId: profile.id })
+
+            if(user){
+                done(null, user)
+            } else {
+                user = await User.create(newUser)
+                done(null, user)
+            }
+        }catch (err){
+            console.error(err)
+        }
     }))
     passport.serializeUser((user, done) => {
         done(null, user.id)
     })
-    passport.deserializeUser((id, done) => {
-        User.findById(id, (err, user) => done(err, user))
+    passport.deserializeUser(async (id, done) => {
+        // User.findById(id, (err, user) => done(err, user))
+        try{
+            const user = await User.findById(id)
+            done(null, user)
+        } catch (err) {
+            done(err)
+        }
     })
 }
